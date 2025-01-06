7bc3942ac82ff7c7bd64fe180e0e141770511e2e
Tiffany Timbers <tiffany.timbers@gmail.com>
2025-01-06 11:00:17 -0800

    added pycounts_tt25 to namespace

--- src/pycounts_tt25/__init__.py
+++ src/pycounts_tt25/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("pycounts_tt25")
+
+
+from pycounts_tt25.pycounts_tt25 import count_words
+from pycounts_tt25.plotting import plot_words
