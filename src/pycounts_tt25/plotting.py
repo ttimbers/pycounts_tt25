3554d0562a611246899ccbc98c9574db135ed935
Tiffany Timbers <tiffany.timbers@gmail.com>
2025-01-17 13:42:39 -0800

    example of visual regression test for matplotlib

--- src/pycounts_tt25/plotting.py
+++ src/pycounts_tt25/plotting.py
@@ -24,8 +24,8 @@ def plot_words(word_counts, n=10):
     """
     top_n_words = word_counts.most_common(n)
     word, count = zip(*top_n_words)
-    fig = plt.bar(range(n), count)
+    plt.bar(range(n), count)
     plt.xticks(range(n), labels=word, rotation=45)
     plt.xlabel("Word")
     plt.ylabel("Count")
-    return fig
+    return plt.gcf()
